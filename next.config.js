8726be65837f6a1eaa236c8286c152175f89887e
Charlson So <charlsonsoo@gmail.com>
2024-10-21 22:40:38 -0700

    Added export command to generate static webpages for github pages

--- next.config.js
+++ next.config.js
@@ -1,1 +1,9 @@
-module.exports = {};
+module.exports = {
+  // Ensures exported pages work correctly when deployed to static hosting
+  exportTrailingSlash: true,
+
+  // Disable image optimization if necessary (since GitHub Pages does not support Next.js' image optimization)
+  images: {
+    unoptimized: true,
+  },
+};
